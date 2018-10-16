6df472e7357adc13e24fbe34315ac607fc9b022b
GeruN <33115757+GerunNik@users.noreply.github.com>
2018-10-16 16:08:08 +0200

    Add files via upload

---
+++ DemoBasicAuth/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoBasicAuth
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Login()
+        {
+            UserName = "Nikola";
+            Password = "Gerun";
+        }
+        public Login(string userValue, string passwordValue)
+        {
+            UserName = userValue;
+            Password = passwordValue;
+        }
+    }
+}
