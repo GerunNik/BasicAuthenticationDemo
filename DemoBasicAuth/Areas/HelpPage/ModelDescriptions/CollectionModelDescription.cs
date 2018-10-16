ad995baa0bb057d66b385a00325c7faef058a68f
GeruN <33115757+GerunNik@users.noreply.github.com>
2018-10-16 16:10:52 +0200

    Add files via upload

---
+++ DemoBasicAuth/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DemoBasicAuth.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DemoBasicAuth/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DemoBasicAuth.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
